e3db48c6bdfcb0eecf35fd99a44b303257d36d52
Vishal Kaushal <vishalkaushalit@gmail.com>
2026-07-24 09:49:10 +0530

    .

--- android/app/.cxx/Debug/f28141a5/prefab/armeabi-v7a/prefab/lib/arm-linux-androideabi/cmake/react-native-reanimated/react-native-reanimatedConfig.cmake
+++ android/app/.cxx/Debug/f28141a5/prefab/armeabi-v7a/prefab/lib/arm-linux-androideabi/cmake/react-native-reanimated/react-native-reanimatedConfig.cmake
@@ -1,7 +1,6 @@
 if(NOT TARGET react-native-reanimated::reanimated)
-add_library(react-native-reanimated::reanimated SHARED IMPORTED)
+add_library(react-native-reanimated::reanimated INTERFACE IMPORTED)
 set_target_properties(react-native-reanimated::reanimated PROPERTIES
-    IMPORTED_LOCATION "/Applications/XAMPP/xamppfiles/htdocs/Github/zippie_mobile_app/node_modules/react-native-reanimated/android/build/intermediates/cxx/Debug/2z663q4t/obj/armeabi-v7a/libreanimated.so"
     INTERFACE_INCLUDE_DIRECTORIES "/Applications/XAMPP/xamppfiles/htdocs/Github/zippie_mobile_app/node_modules/react-native-reanimated/android/build/prefab-headers/reanimated"
     INTERFACE_LINK_LIBRARIES ""
 )
